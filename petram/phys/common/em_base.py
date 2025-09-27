ab13663fd1c91f223d7313ef60709e4d3f308902
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2025-09-27 16:14:55 -0400

    introduced EMPhysModule baseclass

--- petram/phys/common/em_base.py
+++ petram/phys/common/em_base.py
@@ -1,26 +1,26 @@
 #
 #  common routine for EM Physics Modules
 #
+from petram.phys.phys_model import PhysModule
 import numpy as np
 
 import petram.debug as debug
 dprint1, dprint2, dprint3 = debug.init_dprints('EMPhysModule')
 
-from petram.phys.phys_model import PhysModule
 
 class EMPhysModule(PhysModule):
     def __init__(self, **kwargs):
         super(EMPhysModule, self).__init__(**kwargs)
         self.freq_txt = "1.0e9"
-        
+
     def attribute_set(self, v):
         v = super(EMPhysModule, self).attribute_set(v)
         v["freq_txt"] = "1.0e9"
         return v
-        
+
     def is_complex(self):
         return True
-    
+
     def get_default_ns(self):
         from petram.phys.phys_const import mu0, epsilon0, q0, massu, chargez
         ns = {'mu0': mu0,
@@ -41,19 +41,17 @@ class EMPhysModule(PhysModule):
 
     def panel1_param(self):
         panels = super(EMPhysModule, self).panel1_param()
-        panels.extend([self.make_param_panel('freq',  self.freq_txt),])
+        panels.extend([self.make_param_panel('freq',  self.freq_txt), ])
 
         return panels
-    
 
-    def get_panel1_value(self):    
+    def get_panel1_value(self):
         val = super(EMPhysModule, self).get_panel1_value()
-        val.extend([self.freq_txt,])
-        
+        val.extend([self.freq_txt, ])
+
         return val
 
     def import_panel1_value(self, v):
         v = super(EMPhysModule, self).import_panel1_value(v)
         self.freq_txt = str(v[0])
         return v[1:]
-    
